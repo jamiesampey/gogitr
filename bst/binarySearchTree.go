aff4b38411f463f8e4044b4b26a351dcb190306e
Jamie Sampey <jamie.sampey@gmail.com>
2023-03-29 13:58:43 -0600

    Rework bst to rebalance and introduce Node struct

--- bst/binarySearchTree.go
+++ bst/binarySearchTree.go
@@ -7,64 +7,22 @@ import (
 )
 
 type BST struct {
-	root *node
-}
-
-type node struct {
-	value, balance int
-	left           *node
-	right          *node
-}
-
-func newLeafNode(value int) *node {
-	return &node{value, 0, nil, nil}
+	root *Node
 }
 
 func (bst *BST) PopulateTree(numNodes int) {
 	numInserted := 0
+	var err error
 	for numInserted < numNodes {
-		insertSuccess := bst.insertNode(rand.Intn(100))
-		if insertSuccess {
+		num := rand.Intn(100)
+		bst.root, err = bst.root.Insert(num)
+		if err == nil {
 			numInserted++
+			log.Printf("Inserted %d | Tree height %d | Tree balance %d",
+				num, bst.root.height, bst.root.Balance())
+		} else {
+			fmt.Println(err)
 		}
-	}
-}
-
-func (bst *BST) insertNode(value int) bool {
-	if bst.root == nil {
-		log.Printf("Inserting %d at ROOT", value)
-		bst.root = newLeafNode(value)
-		return true
-	}
-
-	currentNode := bst.root
-	for {
-		if value == currentNode.value {
-			logInsertOp(value, currentNode.value)
-			return false
-		}
-
-		if value < currentNode.value {
-			if currentNode.left != nil {
-				currentNode.balance--
-				currentNode = currentNode.left
-				continue
-			}
-
-			logInsertOp(value, currentNode.value)
-			currentNode.left = newLeafNode(value)
-			return true
-		}
-
-		if currentNode.right != nil {
-			currentNode.balance++
-			currentNode = currentNode.right
-			continue
-		}
-
-		logInsertOp(value, currentNode.value)
-		currentNode.right = newLeafNode(value)
-		return true
 	}
 }
 
@@ -73,7 +31,7 @@ func (bst *BST) PrintSortedTree() {
 	fmt.Println()
 }
 
-func (bst *BST) printSubTree(n *node) {
+func (bst *BST) printSubTree(n *Node) {
 	if n.left != nil {
 		bst.printSubTree(n.left)
 	}
@@ -87,7 +45,7 @@ func (bst *BST) printSubTree(n *node) {
 
 func logInsertOp(newValue, lastValue int) {
 	if newValue == lastValue {
-		log.Printf("NO OP - BST already contains %d", newValue)
+
 		return
 	}
 
---
+++ bst/node.go
@@ -0,0 +1,124 @@
+package bst
+
+import (
+	"fmt"
+)
+
+type Node struct {
+	value, height int
+	left          *Node
+	right         *Node
+}
+
+func (node *Node) Insert(val int) (*Node, error) {
+
+	if node == nil {
+		return &Node{value: val, height: 1}, nil
+	}
+
+	if node.value == val {
+		return node, fmt.Errorf("node %d already exists", val)
+	}
+
+	var err error
+	if val < node.value {
+		node.left, err = node.left.Insert(val)
+	} else {
+		node.right, err = node.right.Insert(val)
+	}
+
+	if err == nil {
+		node.height = node.maxChildHeight() + 1
+		return node.rebalance(), nil
+	}
+
+	return node, err
+}
+
+func (node *Node) Balance() int {
+	if node == nil {
+		return 0
+	}
+	return node.rightHeight() - node.leftHeight()
+}
+
+func (node *Node) rebalance() *Node {
+	switch {
+
+	// shift LEFT cases
+	case node.Balance() > 1 && node.right.Balance() == 1:
+		// node is too heavy on the right, and it's right child is weighted right
+		return node.shiftLeft()
+	case node.Balance() > 1 && node.right.Balance() == -1:
+		// node is too heavy on the right, and it's right child is weighted left
+		node.right = node.right.shiftRight()
+		node = node.shiftLeft()
+		node.height = node.maxChildHeight() + 1
+		return node
+
+	// shift RIGHT cases
+	case node.Balance() < -1 && node.left.Balance() == -1:
+		// node is too heavy on the left, and it's left child is weighted left
+		return node.shiftRight()
+	case node.Balance() < -1 && node.left.Balance() == 1:
+		// node is too heavy on the left, and it's left child is weighted right
+		node.left = node.left.shiftLeft()
+		node = node.shiftRight()
+		node.height = node.maxChildHeight() + 1
+		return node
+	}
+
+	return node // no rebalance needed
+}
+
+// The right child SHIFTS UP AND LEFT to become the parent node
+// The orig parent node SHIFTS DOWN AND LEFT to become the new parent's left node
+// The orig right child's left must move over to the new left child's right
+func (node *Node) shiftLeft() *Node {
+	origRight := node.right
+	node.right = origRight.left
+	origRight.left = node
+
+	node.height = node.maxChildHeight() + 1
+	origRight.height = origRight.maxChildHeight() + 1
+
+	return origRight // origRight becomes the new parent
+}
+
+// The left child shifts UP and RIGHT to become the parent node
+// The orig parent node shifts DOWN and RIGHT to become the new parent's right node
+// The orig left child's right must move over to the new right child's left
+func (node *Node) shiftRight() *Node {
+	origLeft := node.left
+	node.left = origLeft.right
+	origLeft.right = node
+
+	node.height = node.maxChildHeight() + 1
+	origLeft.height = origLeft.maxChildHeight() + 1
+
+	return origLeft // origLeft becomes the new parent
+}
+
+func (node *Node) maxChildHeight() int {
+	leftHeight := node.leftHeight()
+	rightHeight := node.rightHeight()
+
+	if leftHeight > rightHeight {
+		return leftHeight
+	}
+	return rightHeight
+}
+
+func (node *Node) leftHeight() int {
+	if node.left == nil {
+		return 0
+	}
+	return node.left.height
+}
+
+func (node *Node) rightHeight() int {
+	if node.right == nil {
+		return 0
+	}
+	return node.right.height
+}
