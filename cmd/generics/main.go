d2d2cd64ad01e8071fcd932da9390fa170ec8ad4
Jamie Sampey <jamie.sampey@gmail.com>
2023-04-24 21:29:07 -0600

    iterate through popping the generic stack

--- cmd/generics/main.go
+++ cmd/generics/main.go
@@ -4,18 +4,22 @@ import (
 	"fmt"
 	"jamiesampey.com/gogitr/pkg/generics"
 	"jamiesampey.com/gogitr/pkg/generics/ski"
-	"log"
 )
 
 func main() {
-	tourStack := new(generics.Stack[ski.Ski])
-	tourStack.Push(ski.New("DPS", "Pagoda Tour", 179, 106, 19.0))
-	tourStack.Push(ski.New("Blizzard", "Rustler 11", 180, 112, 19.1))
-	tourStack.Push(ski.New("Salomon", "QST Stella", 173, 106, 16.2))
+	skiStack := new(generics.Stack[ski.Ski])
+	skiStack.Push(ski.New("DPS", "Pagoda Tour", 179, 106, 19.0))
+	skiStack.Push(ski.New("Blizzard", "Rustler 11", 180, 112, 19.1))
+	skiStack.Push(ski.New("Salomon", "QST Stella", 173, 106, 16.2))
 
-	item, err := tourStack.Pop()
-	if err != nil {
-		log.Fatal("Received error on pop")
+	//var err error
+	for {
+		item, err := skiStack.Pop()
+		if err != nil {
+			fmt.Printf("err is '%v'\n", err)
+			break
+		}
+		fmt.Printf("Popped item is %s\n", item.String())
 	}
-	fmt.Printf("Popped item is %s", item.String())
+
 }
